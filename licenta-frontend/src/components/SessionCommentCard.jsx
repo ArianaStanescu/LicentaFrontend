2ed2775259fbc479c380d62c9cf87e3509d24b0d
ArianaStanescu <stanescu.ariana02@gmail.com>
2025-04-12 14:01:37 +0300

    enhancements to session comment card

--- licenta-frontend/src/components/SessionCommentCard.jsx
+++ licenta-frontend/src/components/SessionCommentCard.jsx
@@ -1,20 +1,45 @@
 import React from "react";
-import { Box, Button, Card, CardContent, Typography } from "@mui/material";
+import { Box, Card, CardContent, Typography } from "@mui/material";
+import { getParentId, getTrainerId } from "../helpers/localStorageHelper";
 
-export const SessionCommentCard = ({ comment, sessionId }) => {
+export const SessionCommentCard = ({ comment }) => {
 
+    const isAuthor = () => {
+        return comment?.authorParent?.id == getParentId() || comment?.authorTrainer?.id == getTrainerId();
+    }
 
     return (
-        <Card sx={{
-            width: "100%",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-        }}>
+        <Card sx={{ width: "100%", mb: 2 }}>
             <CardContent>
-                <Typography>
-                    {comment?.content}
-                </Typography>
+                <Box display="flex" justifyContent="space-between" alignItems="flex-start">
+                    <Box mr={2} minWidth={150} flexDirection={"column"} display={"flex"} gap={0.5}>
+                        <Box display="flex" alignItems="center" gap={0.5}>
+                            <Typography variant="subtitle1" fontWeight="bold">
+                                {comment?.authorTrainer ? `${comment?.authorTrainer?.firstName} ${comment?.authorTrainer?.lastName}` : `${comment?.authorParent?.firstName} ${comment?.authorParent?.lastName}`}
+                            </Typography>
+                            {isAuthor() &&
+                                <Typography variant="subtitle1" fontWeight="bold" sx={{ fontStyle: 'italic' }}>
+                                    (Tu)
+                                </Typography>
+                            }
+                        </Box>
+                        <Typography variant="subtitle2" fontWeight="bold">
+                            {comment?.authorParent ? "Parinte" : "Trainer"}
+                        </Typography>
+                        <Typography variant="subtitle2" color="text.primary">
+                            {new Date(comment?.createdAt).toLocaleString("ro-RO")}
+                        </Typography>
+                        <Typography variant="subtitle2" color="red" sx={{ fontStyle: 'italic' }}>
+                            (Nou)
+                        </Typography>
+                    </Box>
+
+                    <Box flexGrow={1}>
+                        <Typography>
+                            {comment?.content}
+                        </Typography>
+                    </Box>
+                </Box>
             </CardContent>
         </Card>
     );
